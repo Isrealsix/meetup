f6ec1a67b25a0cb7067cfa69a24cf77e0aec1921
Isreal6ix <isrealsix@gmail.com>
2021-08-06 22:02:04 +0700

    added getStaticPaths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9713153bb0f509b5bf23";
+/******/ 	var hotCurrentHash = "cb0affb8b4da9fdc1f4b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -8,4 +8,39 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: true,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId
+    // fetch data for a single meetup
+    return {
+        props: {
+            meetupData: {
+                image: 'https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80',
+                id: 'm1',
+                title: 'A First Meetup',
+                address: '1414 becker st.',
+                description: 'The meetup description'
+            }
+        }
+    }
+}
+
+
 export default MeetupDetails
