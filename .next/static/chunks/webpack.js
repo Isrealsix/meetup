77fb0c43024dc6541d5761e690be25fdb67109b2
Isreal6ix <isrealsix@gmail.com>
2021-08-20 23:06:59 +0700

    added s=some featyres

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8498716f69f21a79f327";
+/******/ 	var hotCurrentHash = "449adcb2001df82d1508";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
